6d2c65edca17f6bf39e2b3da57bf5021975370fd
Mohammed Meraj <mehrajmohd00@gmail.com>
2025-09-09 23:18:22 +0530

    Add files via upload

---
+++ Python_learning/comparisonoperators.py
@@ -0,0 +1,20 @@
+#there are six types of comparison operators - >,<,>=,<=, ==, !=
+
+# a = 12.1 
+# b = 12
+
+# print(a == b)
+# print(a != b)
+
+# print(a > b)
+
+# print(45<67)
+
+# print(23 >= 23)
+# print(45 <= 45)
+
+print(ord("A"))
+print(ord("B"))
+
+print("A" > "B")
+
